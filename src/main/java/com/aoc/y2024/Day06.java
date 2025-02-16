5941fd1dfcad5a7e06abf4ac02744b8f923021bd
weixuanteo <weixuanteo@gmail.com>
2025-02-17 00:30:25 +0800

    add day 6 part 2 2024
    * fix direction changing code (did not account for edge case where direction change can be > 90 degrees in the same position)
    * include intellj files to .gitignore

--- src/main/java/com/aoc/y2024/Day06.java
+++ src/main/java/com/aoc/y2024/Day06.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.nio.*;
 import java.nio.file.*;
 import java.util.*;
 import java.util.stream.*;
@@ -14,6 +13,15 @@ public class Day06 {
             put('>', new int[] { 0, 1 });
             put('V', new int[] { 1, 0 });
             put('<', new int[] { 0, -1 });
+        }
+    };
+
+    private Map<Character, Character> directionsMarkerMap = new HashMap<>() {
+        {
+            put('^', '|');
+            put('>', '-');
+            put('V', '|');
+            put('<', '-');
         }
     };
 
@@ -43,7 +51,6 @@ public class Day06 {
         int positions = 0;
         for (int y = 0; y < map.size(); y++) {
             String line = mapCopy.get(y);
-            System.out.println(line);
             for (int x = 0; x < line.length(); x++) {
                 if (line.charAt(x) == 'X') {
                     positions += 1;
@@ -54,21 +61,73 @@ public class Day06 {
     }
 
     private Object part2() {
-        return 0;
+        List<String> map = getInput().toList();
+        List<String> mapCopy = new ArrayList<>(map);
+        int[] startPosition = new int[2];
+
+        for (int y = 0; y < map.size(); y++) {
+            String line = map.get(y);
+            for (int x = 0; x < line.length(); x++) {
+                if (line.charAt(x) == '^') {
+                    startPosition[0] = y;
+                    startPosition[1] = x;
+
+                    getGuardPath(mapCopy, new int[] { y, x });
+                }
+            }
+        }
+
+        List<int[]> obstaclePositions = new ArrayList<>();
+        for (int y = 0; y < map.size(); y++) {
+            String line = mapCopy.get(y);
+            for (int x = 0; x < line.length(); x++) {
+                if (line.charAt(x) == 'X') {
+                    obstaclePositions.add((new int[]{y, x}));
+                }
+            }
+        }
+
+
+        int loops = 0;
+        for (int[] obstaclePosition : obstaclePositions) {
+            if (Arrays.equals(obstaclePosition, startPosition)) {
+                continue;
+            }
+            mapCopy = new ArrayList<>(map);
+            markPosition(mapCopy, obstaclePosition, '$');
+            boolean isLoop = getGuardPath(mapCopy, startPosition);
+            if (isLoop) {
+                loops++;
+            }
+        }
+
+        return loops;
     }
 
-    private void getGuardPath(List<String> map, int[] startPosition) {
+    private void printMap(List<String> map) {
+        for (String line : map) {
+            System.out.println(line);
+        }
+    }
+    private boolean getGuardPath(List<String> map, int[] startPosition) {
         int[] currentPosition = Arrays.copyOf(startPosition, 2);
         char currentDirectionSymbol = map
             .get(startPosition[0])
             .charAt(startPosition[1]);
         int[] currentDirection = directionsMap.get(currentDirectionSymbol);
+        int visitedCount = 0;
 
         while (
             isValidPosition(currentPosition, map.size(), map.get(0).length())
         ) {
             // mark current position with X
             markPosition(map, currentPosition, 'X');
+
+            visitedCount++;
+            // pretty hack way to check if it's a loop (maybe there's a better deterministic way?)
+            if (visitedCount > 20000) {
+                return true;
+            }
 
             // look ahead to check for obstacle
             int[] nextPosition = Arrays.copyOf(currentPosition, 2);
@@ -77,19 +136,82 @@ public class Day06 {
             if (
                 !isValidPosition(nextPosition, map.size(), map.get(0).length())
             ) {
-                return;
+                return false;
             }
-            if (map.get(nextPosition[0]).charAt(nextPosition[1]) == '#') {
+            while (isObstacle(map.get(nextPosition[0]).charAt(nextPosition[1]))) {
                 // change direction
                 currentDirectionSymbol = getNextDirection(
                     currentDirectionSymbol
                 );
                 currentDirection = directionsMap.get(currentDirectionSymbol);
+                nextPosition[0] = currentPosition[0] + currentDirection[0];
+                nextPosition[1] = currentPosition[1] + currentDirection[1];
             }
 
             currentPosition[0] += currentDirection[0];
             currentPosition[1] += currentDirection[1];
         }
+        return false;
+    }
+
+
+    // same logic as getGuardPath but with guard position direction marking for debugging purposes
+    private boolean getGuardPath2(List<String> map, int[] startPosition) {
+        int[] currentPosition = Arrays.copyOf(startPosition, 2);
+        char currentDirectionSymbol = map
+            .get(startPosition[0])
+            .charAt(startPosition[1]);
+        int[] currentDirection = directionsMap.get(currentDirectionSymbol);
+        char currentDirectionMarker = '|';
+
+        int visitedCount = 0;
+
+        while (
+            isValidPosition(currentPosition, map.size(), map.get(0).length())
+        ) {
+            // mark current position with directions of where the guard has gone
+            if (!Arrays.equals(currentPosition, startPosition)) {
+                markPosition(map, currentPosition, currentDirectionMarker);
+            }
+
+            visitedCount++;
+            // pretty hack way to check if it's a loop (maybe there's a better deterministic way?)
+            if (visitedCount > 20000) {
+                return true;
+            }
+
+
+            // look ahead to check for obstacle
+            int[] nextPosition = Arrays.copyOf(currentPosition, 2);
+            nextPosition[0] += currentDirection[0];
+            nextPosition[1] += currentDirection[1];
+            if (
+                !isValidPosition(nextPosition, map.size(), map.get(0).length())
+            ) {
+                return false;
+            }
+            while (isObstacle(map.get(nextPosition[0]).charAt(nextPosition[1]))) {
+                // change direction
+                currentDirectionSymbol = getNextDirection(
+                    currentDirectionSymbol
+                );
+                currentDirection = directionsMap.get(currentDirectionSymbol);
+                markPosition(map, currentPosition, '+');
+                currentDirectionMarker = directionsMarkerMap.get(
+                    currentDirectionSymbol
+                );
+                nextPosition[0] = currentPosition[0] + currentDirection[0];
+                nextPosition[1] = currentPosition[1] + currentDirection[1];
+            }
+
+            currentPosition[0] += currentDirection[0];
+            currentPosition[1] += currentDirection[1];
+        }
+        return false;
+    }
+
+    private boolean isObstacle(char current) {
+        return current == '#' || current == '$';
     }
 
     private void markPosition(List<String> map, int[] position, char marker) {
